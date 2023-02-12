9786e09a00e3ca86cf9f1ad24dbb6115a596de76
Timdu <timdu365@gmail.com>
2023-02-12 22:50:31 +0800

    add production comment

--- webpack.react.host.js
+++ webpack.react.host.js
@@ -1,4 +1,3 @@
-const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
     template: './index.html',
